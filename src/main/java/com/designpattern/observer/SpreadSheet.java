43c2c0d388e69f2e210b6105e55c11608a43f341
vaibhav <bomlevaihav@gmail.com>
2025-03-11 09:33:27 +0530

    implemented observer pattern

---
+++ src/main/java/com/designpattern/observer/SpreadSheet.java
@@ -0,0 +1,10 @@
+package com.designpattern.observer;
+
+public class SpreadSheet implements Observer{
+
+    @Override
+    public void update() {
+      System.out.println("Spreadsheet got updated");
+    }
+
+}
