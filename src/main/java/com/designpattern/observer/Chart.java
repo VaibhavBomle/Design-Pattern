7d23d71378dbc388562d5b623d8c5e4c62978b0c
vaibhav <bomlevaihav@gmail.com>
2025-03-15 12:50:02 +0530

    Updated observer pattern with pull style

--- src/main/java/com/designpattern/observer/Chart.java
+++ src/main/java/com/designpattern/observer/Chart.java
@@ -1,8 +1,14 @@
 package com.designpattern.observer;
 
 public class Chart implements Observer{
+
+    private DataSource dataSource;
+
+    public Chart(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
     @Override
     public void update() {
-      System.out.println("Chart got updated");
+      System.out.println("Chart got updated :"+dataSource.getValue());
     }
 }
--- src/main/java/com/designpattern/observer/SpreadSheet.java
+++ src/main/java/com/designpattern/observer/SpreadSheet.java
@@ -2,9 +2,15 @@ package com.designpattern.observer;
 
 public class SpreadSheet implements Observer{
 
+    private DataSource dataSource;
+
+    public SpreadSheet(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Override
     public void update() {
-      System.out.println("Spreadsheet got updated");
+      System.out.println("Spreadsheet got updated" + dataSource.getValue());
     }
 
 }
